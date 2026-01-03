311323be26577b91bec426220cde2035186e7e94
Kanchana EK <kanchanakailas04@gmail.com>
2026-01-03 15:08:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  toggleButton.textContent = document.body.classList.contains('dark-mode') ? '☀' : '🌙';
+});
